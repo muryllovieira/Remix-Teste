0d4f7a221c1d25452957d120b6c3c745966cab35
Muryllo Siqueira Vieira <muryllo.vieira@sp.senai.br>
2025-05-23 10:20:54 -0300

    testando

--- app/routes/auth/sign-in.tsx
+++ app/routes/auth/sign-in.tsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router";
+import { Form, Link } from "react-router";
 import type { Route } from "./+types/sign-in";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Sign In" }];
 }
 
+export async function action({ request }: Route.ActionArgs) {
+  const data = await request.formData();
+
+  const email = data.get("email");
+  const password = data.get("password");
+
+  console.log({ email, password });
+}
+
 export default function SignIn() {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-4xl font-bold">Sign In</h1>
+
+      <Form method="post">
+        <input type="email" name="email" id="email" placeholder="Email" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Senha"
+        />
+        <button type="submit">Entrar</button>
+      </Form>
 
       <Link to="/sign-up">Create an account</Link>
     </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Repository } from "../http/types/github-repositories";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export async function loader({}: Route.LoaderArgs) {
+  const response = await fetch(
+    "https://api.github.com/users/muryllovieira/repos"
+  );
+  const data: Repository[] = await response.json();
+
+  return { repositories: data };
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { repositories } = loaderData;
+
+  return (
+    <div className="h-dvh bg-zinc-950 text-white">
+      <ul>
+        {repositories.map((repo) => {
+          return <li key={repo.id}>{repo.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
